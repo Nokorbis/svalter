ab76c79075df31ba17dae75105925ad59aed16c3
Nokorbis <nokorbis@gmail.com>
2020-07-15 19:11:25 +0200

    Separate style on blog/slug

--- generators/app/templates/sapper/src/routes/blog/[slug].svelte
+++ generators/app/templates/sapper/src/routes/blog/[slug].svelte
@@ -17,41 +17,6 @@
 	export let post;
 </script>
 
-<style>
-	/*
-		By default, CSS is locally scoped to the component,
-		and any unused styles are dead-code-eliminated.
-		In this page, Svelte can't know which elements are
-		going to appear inside the {{{post.html}}} block,
-		so we have to use the :global(...) modifier to target
-		all elements inside .content
-	*/
-	.content :global(h2) {
-		font-size: 1.4em;
-		font-weight: 500;
-	}
-
-	.content :global(pre) {
-		background-color: #f9f9f9;
-		box-shadow: inset 1px 1px 5px rgba(0,0,0,0.05);
-		padding: 0.5em;
-		border-radius: 2px;
-		overflow-x: auto;
-	}
-
-	.content :global(pre) :global(code) {
-		background-color: transparent;
-		padding: 0;
-	}
-
-	.content :global(ul) {
-		line-height: 1.5;
-	}
-
-	.content :global(li) {
-		margin: 0 0 0.5em 0;
-	}
-</style>
 
 <svelte:head>
 	<title>{post.title}</title>
@@ -62,3 +27,25 @@
 <div class='content'>
 	{@html post.html}
 </div>
+
+<% if (separation) { -%>
+
+<% if (sass) { -%>
+<style src="./_slug.scss"></style>
+<% } else { -%>
+<style src="./_slug.css"></style>
+<% } -%>
+
+<% } else { -%>
+
+<% if (sass) { -%>
+<style lang="scss">
+  <%- include('../../../../_specificities/sapper/sass/src/routes/blog/_slug.scss'); -%>
+</style>
+<% } else { -%>
+<style>
+  <%- include('../../../../_specificities/sapper/css/src/routes/blog/_slug.css'); -%>
+</style>
+<% } -%>
+
+<% } -%>
