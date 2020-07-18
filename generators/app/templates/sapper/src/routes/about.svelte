82b7fd3652cd82686d410cdaf8ddf17ef603bbbc
Nokorbis <nokorbis@gmail.com>
2020-07-18 19:30:32 +0200

    Separate style on about page

--- generators/app/templates/sapper/src/routes/about.svelte
+++ generators/app/templates/sapper/src/routes/about.svelte
@@ -5,3 +5,25 @@
 <h1>About this site</h1>
 
 <p>This is the 'about' page. There's not much here.</p>
+
+<% if (separation) { -%>
+
+<% if (sass) { -%>
+<style src="./_about.scss"></style>
+<% } else { -%>
+<style src="./_about.css"></style>
+<% } -%>
+
+<% } else { -%>
+
+<% if (sass) { -%>
+<style lang="scss">
+  <%- include('../../../_specificities/sapper/sass/src/routes/_about.scss'); -%>
+</style>
+<% } else { -%>
+<style>
+  <%- include('../../../_specificities/sapper/css/src/routes/_about.css'); -%>
+</style>
+<% } -%>
+
+<% } -%>
