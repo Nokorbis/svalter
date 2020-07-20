05b9a8ca70342daad4d63585603c7854cc6ce2e5
Nokorbis <nokorbis@gmail.com>
2020-07-20 19:07:45 +0200

    global scss on svelte projects

--- generators/app/templates/svelte/src/App.svelte
+++ generators/app/templates/svelte/src/App.svelte
@@ -1,33 +1,16 @@
 <script>
+  import Layout from 'Layout.svelte';
   export let name;
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Layout {name} />
 
-<% if (separation) { -%>
-
-  <% if (sass) { -%>
-<style src="./App.scss"></style>
-  <% } else { -%>
-<style src="./App.css"></style>
-  <% } -%>
-
-<% } else { -%>
-
-  <% if (sass) { -%>
-<style lang="scss">
-<%- include('../../_specificities/svelte/sass/src/App.scss'); -%>
+<% if (sass) { -%>
+<style lang="scss" global>
+  @import 'assets/styles/global.scss';
 </style>
-  <% } else { -%>
-<style>
-<%- include('../../_specificities/svelte/css/src/App.css'); -%>
-</style>
-  <% } -%>
-
 <% } -%>
+
 
 
 
---
+++ generators/app/templates/svelte/src/Layout.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let name;
+</script>
+
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+
+<% if (separation) { -%>
+
+<% if (sass) { -%>
+<style src="./_Layout.scss"></style>
+<% } else { -%>
+<style src="./_Layout.css"></style>
+<% } -%>
+
+<% } else { -%>
+
+<% if (sass) { -%>
+<style lang="scss">
+  <%- include('../../_specificities/svelte/sass/src/_Layout.scss'); -%>
+</style>
+<% } else { -%>
+<style>
+  <%- include('../../_specificities/svelte/css/src/_Layout.css'); -%>
+</style>
+<% } -%>
+
+<% } -%>
