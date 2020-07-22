f53b92cedc58d86895eb9b372c2e9ba23f296170
Nokorbis <nokorbis@gmail.com>
2020-07-22 18:29:13 +0200

    Use extracted script on svelte

--- generators/app/templates/svelte/src/App.svelte
+++ generators/app/templates/svelte/src/App.svelte
@@ -1,24 +1,4 @@
-<% if (separation) { -%>
-
-<% if (typescript) { -%>
-<script src="./_App.ts"></script>
-<% } else { -%>
-<script src="./_App.js"></script>
-<% } -%>
-
-<% } else { -%>
-
-<% if (typescript) { -%>
-<script lang="ts">
-  <%- include('../../_specificities/svelte/typescript/separation/src/_App.ts'); -%>
-</script>
-<% } else { -%>
-<script>
-  <%- include('../../_specificities/svelte/javascript/separation/src/_App.js'); -%>
-</script>
-<% } -%>
-
-<% } -%>
+<%- include (paths.partials.scripts, {filename: '_App', folder: 'src'}); %>
 
 <Layout {name} />
 
