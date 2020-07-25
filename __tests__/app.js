22077a1c313857cb68739c94394e185adc0f8062
Nokorbis <nokorbis@gmail.com>
2020-07-25 22:01:26 +0200

    second unit test

--- __tests__/app.js
+++ __tests__/app.js
@@ -6,30 +6,45 @@ const helpers = require('yeoman-test');
 describe('generator-svalter:app', () => {
     describe('simple svelte', function() {
         const simpleSvelteScenario = require('./_scenarios/simple-svelte.json');
-        beforeAll(() => {
-            return helpers
-                .run(path.join(__dirname, '../generators/app'))
-                .withPrompts(simpleSvelteScenario.choices);
-        });
+        executeScenario(simpleSvelteScenario);
+    });
 
-        it('should create files', () => {
-            assert.file(simpleSvelteScenario.filesToCreate);
-        });
-
-        it('should not create some files', () => {
-            assert.noFile(simpleSvelteScenario.filesNotToCreate);
-        });
-
-        it('should contain some data', () => {
-            for (let entry of simpleSvelteScenario.filesContent) {
-                assert.fileContent(entry.filePath, entry.content);
-            }
-        });
-
-        it('should not contain some data', () => {
-            for (let entry of simpleSvelteScenario.filesNoContent) {
-                assert.noFileContent(entry.filePath, entry.content);
-            }
-        });
+    describe('complete svelte', function() {
+        const scenario = require('./_scenarios/complete-svelte.json');
+        executeScenario(scenario);
     });
 });
+
+function executeScenario(scenario) {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts(scenario.choices);
+  });
+
+  if (scenario.filesToCreate) {
+    it('should create files', () => {
+      assert.file(scenario.filesToCreate);
+    });
+  }
+
+  if (scenario.filesNotToCreate) {
+    it('should not create some files', () => {
+      assert.noFile(scenario.filesNotToCreate);
+    });
+  }
+
+  if (scenario.filesContent) {
+    it('should contain some data', () => {
+      for (let entry of scenario.filesContent) {
+        assert.fileContent(entry.filePath, entry.content);
+      }
+    });
+  }
+
+  if (scenario.filesNoContent) {
+    it('should not contain some data', () => {
+      for (let entry of scenario.filesNoContent) {
+        assert.noFileContent(entry.filePath, entry.content);
+      }
+    });
+  }
+}
