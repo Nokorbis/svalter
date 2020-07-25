1232644c11c6a686217f9379f27a35e89861c3e7
Nokorbis <nokorbis@gmail.com>
2020-07-25 21:35:27 +0200

    first unit test

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,13 +4,32 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 describe('generator-svalter:app', () => {
-  beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
-  });
+    describe('simple svelte', function() {
+        const simpleSvelteScenario = require('./_scenarios/simple-svelte.json');
+        beforeAll(() => {
+            return helpers
+                .run(path.join(__dirname, '../generators/app'))
+                .withPrompts(simpleSvelteScenario.choices);
+        });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
-  });
+        it('should create files', () => {
+            assert.file(simpleSvelteScenario.filesToCreate);
+        });
+
+        it('should not create some files', () => {
+            assert.noFile(simpleSvelteScenario.filesNotToCreate);
+        });
+
+        it('should contain some data', () => {
+            for (let entry of simpleSvelteScenario.filesContent) {
+                assert.fileContent(entry.filePath, entry.content);
+            }
+        });
+
+        it('should not contain some data', () => {
+            for (let entry of simpleSvelteScenario.filesNoContent) {
+                assert.noFileContent(entry.filePath, entry.content);
+            }
+        });
+    });
 });
